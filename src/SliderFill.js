107618bb32c2a3b13a590954e62f879d82a1f75a
thoalexander <thoalexander@ebay.com>
2013-08-24 00:30:32 -0700

    commented console.

--- src/SliderFill.js
+++ src/SliderFill.js
@@ -13,14 +13,62 @@ Ext.define('Override.slider.Slider', {
         for (i = 0; i < ln; i++) {
             thumbs[i].getDraggable().setExtraConstraint(null)
                                     .setOffset(newValue[i] * this.offsetValueRatio);
-	    this.fireEvent('change', this,thumbs[i],newValue, oldValue,i);// fire change event based on each thumb
+	        this.fireEvent('change', this,thumbs[i],newValue, oldValue,i);// fire change event based on each thumb
         }
 
         for (i = 0; i < ln; i++) {
-	    
+
             this.refreshThumbConstraints(thumbs[i]);
         }
-	
+
+    },
+    onTap: function(e) {
+
+        if (this.isDisabled()) {
+            return;
+        }
+
+        var targetElement = Ext.get(e.target);
+
+        if (!targetElement || (Ext.browser.engineName == 'WebKit' && targetElement.hasCls('x-thumb'))) {
+            return;
+        }
+
+        var touchPointX = e.touch.point.x,
+            element = this.element,
+            elementX = element.getX(),
+            offset = touchPointX - elementX - (this.thumbWidth / 2),
+            value = this.constrainValue(this.getMinValue() + offset / this.offsetValueRatio),
+            values = this.getValue(),
+            minDistance = Infinity,
+            ln = values.length,
+            i, absDistance, testValue, closestIndex, oldValue, thumb;
+
+        if (ln === 1) {
+            closestIndex = 0;
+        }
+        else {
+            for (i = 0; i < ln; i++) {
+                testValue = values[i];
+                absDistance = Math.abs(testValue - value);
+
+                if (absDistance < minDistance) {
+                    minDistance = absDistance;
+                    closestIndex = i;
+                }
+            }
+        }
+        //console.log(values);
+        //console.log("----------hehe-----------")
+        oldValue = values[closestIndex];
+        thumb = this.getThumb(closestIndex);
+
+        this.setIndexValue(closestIndex, value, this.getAnimation());
+        this.refreshThumbConstraints(thumb);
+
+        if (oldValue !== value) {
+            this.fireEvent('change', this, thumb, value, oldValue);
+        }
     }
 });
 
@@ -35,28 +83,36 @@ Ext.define('Ext.plugin.SliderFill', {
             sliderinner = Ext.get(Ext.DomQuery.select('.x-slider-inner',cmp.element.dom)[0]);
             thumbarr = cmp.getComponent().getThumbs();
 	
-	cmp.on('painted',function(slider){
-	    me.onSliderPainted(cmp);
-	});
+        cmp.on('painted',function(slider){
+            me.onSliderPainted(cmp);
+        });
 	
-	cmp.on('updatedata',function(){
-	    //console.log("updatedata");
-	});
-	cmp.on('change',function(slider,sl,thumb){
-	    var thumbarr_val = slider.getComponent().getThumbs();
-            
-	    Ext.defer(function(){
-		me.thumbAdjust(slider,thumb,thumbarr_val.indexOf(thumb));
-	     },200);
-	});
+        cmp.on('updatedata',function(){
+            //console.log("updatedata");
+        });
+
+
+        cmp.on('dragstart',function(){
+            //console.log("dragstart");
+        });                                    // this, thumb, newValue, oldValue, eOpts
+        cmp.on('change',function(slider,sl,thumb,newVal, oldVal){
+            //console.log(slider);
+            //console.log(sl);
+            var thumbarr_val = slider.getComponent().getThumbs();
+            //me.thumbAdjust(slider,thumb,thumbarr_val.indexOf(thumb));
+            //console.log("-----------------------------------------------");
+            Ext.defer(function(){
+                me.thumbAdjust(slider,thumb,thumbarr_val.indexOf(thumb));
+             },200);
+        });
         
     
 	
-	Ext.Viewport.on('orientationchange',function(){
-	    Ext.defer(function(){
-		me.onSliderPainted(cmp);
-	    },100);
-	});
+        Ext.Viewport.on('orientationchange',function(){
+            Ext.defer(function(){
+                me.onSliderPainted(cmp);
+            },100);
+        });
 	
 	
     },
@@ -67,32 +123,36 @@ Ext.define('Ext.plugin.SliderFill', {
             sliderinner = Ext.get(Ext.DomQuery.select('.x-slider-inner',slider.element.dom)[0]);
 	    //console.log(me);
 	    //console.log(fillCls,"fillCls")
-	Ext.each(thumbarr,function(item,i){
-	    //console.log(item.translatableBehavior.translatable)
-	    var fill_space_id = "fill-"+item.id, width = item.translatableBehavior.translatable.x;
-	    //console.log(fillCls[i],"blasdas" )
-	    Ext.DomHelper.append(sliderinner, {tag: 'div', id: fill_space_id, cls: 'x-slider-fill '+fillCls[i] });
-	    /*if(i > 0){
-		var prev_thumb =  slider.getComponent().getThumb(i-1),
-		    prev_width =  prev_thumb.translatableBehavior.translatable.x,
-		    totalwidth = width - prev_width;
-		Ext.get(fill_space_id).setStyle({width : Math.round(totalwidth)+'px',left : prev_width+'px'});
-	    } else {
-		Ext.get(fill_space_id).setStyle({width : Math.round(width)+'px'});
-	    }*/
-	});
-	slider.getComponent().on({
-	    drag: function(sl,thumb) {
-		//console.log(thumb,"sdfs");
-		var thumbIndex = Ext.Array.indexOf(thumbarr,thumb);
-		me.thumbAdjust(slider,thumb,thumbIndex);
-	    }
-	});
+        Ext.each(thumbarr,function(item,i){
+            //console.log(item.translatableBehavior.translatable)
+            var fill_space_id = "fill-"+item.id, width = item.translatableBehavior.translatable.x;
+            //console.log(fillCls[i],"blasdas" )
+            Ext.DomHelper.append(sliderinner, {tag: 'div', id: fill_space_id, cls: 'x-slider-fill '+fillCls[i] });
+            /*if(i > 0){
+            var prev_thumb =  slider.getComponent().getThumb(i-1),
+                prev_width =  prev_thumb.translatableBehavior.translatable.x,
+                totalwidth = width - prev_width;
+            Ext.get(fill_space_id).setStyle({width : Math.round(totalwidth)+'px',left : prev_width+'px'});
+            } else {
+            Ext.get(fill_space_id).setStyle({width : Math.round(width)+'px'});
+            }*/
+        });
+        slider.getComponent().on({
+            drag: function(sl,thumb) {
+            //console.log(thumb,"sdfs");
+            var thumbIndex = Ext.Array.indexOf(thumbarr,thumb);
+            me.thumbAdjust(slider,thumb,thumbIndex);
+            }
+        });
     },
     thumbAdjust : function(slider,thumb,i){
         var fill_space_id = "fill-"+thumb.getId(),
 	    thumbarr = slider.getComponent().getThumbs(),
             width  = thumb.translatableBehavior.translatable.x;
+        //console.log(thumb.element.getXY());
+        //console.log(thumb.translatableBehavior);
+        //console.log(width,"wow");
+        //width =  thumb.element.getXY()[0];
                 if(i == 0){
                     Ext.get(fill_space_id).setStyle({width : Math.round(width)+'px'});
                     if(slider.getComponent().getThumb(i+1)){
